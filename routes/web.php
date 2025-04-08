0ed186c9960605dcb12385c6535da43e9b105523
ymatsumo <my18yolo7ty@gmail.com>
2025-04-08 18:12:43 +0900

    Add routes for /practice ~ /practice3
    - /practice returns plain string 'practice'
    - /practice2 uses a variable to return 'practice2'
    - /practice3 returns 'test'

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 01:LaravelでRouteを使ってページを追加しよう
+// 「/practice3」にアクセスすると「test」という文字列が返ってくるページ作成
+Route::get('/practice3', function () {
+  return view('test');
+});
+
+// GET /practice でpracticeという文字列を返すこと
+Route::get('/practice', function () {
+  return 'practice';
+});
+
+// GET /practice2 でpractice2という文字列を変数を用いて返すこと
+Route::get('/practice2', function () {
+  $name = 'practice2';
+  return $name;
+});
