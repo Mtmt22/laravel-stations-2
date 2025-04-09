4f38c959487884626a34647d40f162dc027e6750
ymatsumo <my18yolo7ty@gmail.com>
2025-04-09 11:18:57 +0900

    Add PracticeController methods and blade views for /practice, /practice2 and /practice3
    to return '練習' and testParam values

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -4,21 +4,34 @@ namespace App\Http\Controllers;
 
 class PracticeController extends Controller
 {
-  // public function index(){}の塊を メソッド と呼ぶ
-  public function sample()
-  {
-    return response('practice');
+  // 03 HTMLを返すように変更
+  public function sample() {
+    return view('practice');
   }
 
-  public function sample2()
-  {
-    $name = 'practice2';
-    return response($name);
+  public function sample2() {
+    $test = 'practice2';
+    return view('practice2', ['testParam' => $test]);
   }
 
-  public function sample3()
-  {
+  public function sample3() {
     $name = 'test';
-    return response($name);
+    return view('practice3', ['testParam' => $name]);
   }
+
+
+  // 02 public function index(){}の塊を メソッド と呼ぶ
+  // public function sample() {
+  //   return response('practice');
+  // }
+
+  // public function sample2() {
+  //   $name = 'practice2';
+  //   return response($name);
+  // }
+
+  // public function sample3() {
+  //   $name = 'test';
+  //   return response($name);
+  // }
 }
---
+++ resources/views/practice.blade.php
@@ -0,0 +1,23 @@
+<!-- HTML5で書いていると宣言する。
+ 省略しても動くことはあるが必ず書く -->
+<!DOCTYPE html>
+<!-- このHTMLファイルが英語で書かれてますよ。日本語ならja -->
+<html lang="en">
+
+<!-- このページの情報や設定をまとめる -->
+<head>
+  <!-- 文字コードの設定（日本語対応） -->
+  <meta charset="UTF-8">
+  <!-- スマホ・タブレットでもきれいに表示するよ -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Internet Explorer向けに古いブラウザの対策 -->
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- ブラウザのタブに表示されるタイトル（ページの名前） -->
+  <title>Practice</title>
+</head>
+
+<body>
+  <strong>練習</strong>
+</body>
+
+</html>
