cc8790bffb4b8a5a96b0b46c1d086be882782b88
ymatsumo <my18yolo7ty@gmail.com>
2025-04-09 16:39:44 +0900

    Fix api.php
    delete comment

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// 06 映画作品一覧を表示させるページを作成する
